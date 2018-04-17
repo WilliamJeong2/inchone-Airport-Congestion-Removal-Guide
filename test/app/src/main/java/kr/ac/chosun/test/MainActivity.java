6f9eea81063ab6d35f81e9b6656dc1c7b9743ec7
jeon <hun1451@naver.com>
2018-04-18 02:58:12 +0900

    메인화면 적용 및 버튼 기능 수정

--- test/app/src/main/java/kr/ac/chosun/test/MainActivity.java
+++ test/app/src/main/java/kr/ac/chosun/test/MainActivity.java
@@ -18,11 +18,22 @@ import java.net.URL;
 
 public class MainActivity extends Activity {
 
+    //cgtdt 혼잡일자 //조회일자YYYYMMDD
+    //cgthm 혼잡일시 //업데이트시간 HHMM
+    //gate1 2번 출국장 혼잡도
+    //gate2 3번 출국장 혼잡도
+    //gate3 4번 출국장 혼잡도
+    //gate4 5번 출국장 혼잡도
+    //gateinfo1 2번 출국장 대기인원수
+    //gateinfo2 3번 출국장 대기인원수
+    //gateinfo3 4번 출국장 대기인원수
+    //gateinfo4 5번 출국장 대기인원수
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ImageButton congestion = (ImageButton) findViewById(R.id.congestion_button); //해당 버튼을 지정합니다.
         congestion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,7 +79,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        ImageButton convenience = (ImageButton) findViewById(R.id.convenience_button); //해당 버튼을 지정합니다.
+        ImageButton convenience = (ImageButton) findViewById(R.id.convienience_button); //해당 버튼을 지정합니다.
         convenience.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { //버튼이 눌렸을 때
@@ -77,25 +88,22 @@ public class MainActivity extends Activity {
             }
         });
 
+        StrictMode.enableDefaults();
 
+        TextView status1 = (TextView)findViewById(R.id.status1); //파싱된 결과확인!
+        TextView status2 = (TextView)findViewById(R.id.status2);
+        TextView status3 = (TextView)findViewById(R.id.status3);
+        TextView status4 = (TextView)findViewById(R.id.status4);
 
-        /*StrictMode.enableDefaults();
+        boolean initem = false, inAreadiv = false, inCgtdt = false, inCgthm =false, inGateinfo1 = false, inGateinfo2 = false, inGateinfo3 = false, inGateinfo4 = false;
 
-        TextView status1 = (TextView)findViewById(R.id.result); //파싱된 결과확인!
-
-        boolean initem = false, inAtime = false, inT1sum1 = false, inT1sum2 = false, inT1sum3 = false;
-        boolean inT1sum4 = false, inT1sum5 = false, inT1sum6 = false, inT1sum7 = false, inT1sum8=false, inT1sumset1 = false, inT1sumset2 = false;
-        boolean inT2sum1 = false, inT2sum2 = false, inT2sum3 = false, inT2sum4 = false, inT2sumset1 = false, inT2sumset2 = false;
-
-        String atime = null, t1sum1 = null, t1sum2 = null, t1sum3 = null, t1sum4 = null, t1sum5=null, t1sum6=null, t1sum7=null;
-        String t1sum8 = null, t1sumset1 = null, t1sumset2 = null;
-        String t2sum1 = null, t2sum2 = null, t2sum3 = null, t2sum4 = null, t2sumset1 = null, t2sumset2 = null;
+        String areadiv = null, cgtdt = null, cgthm = null, gateinfo1= null, gateinfo2 = null, gateinfo3 = null, gateinfo4 = null;
 
 
         try{
 
-            URL url = new URL("http://openapi.airport.kr/openapi/service/PassengerNoticeKR/getfPassengerNoticeIKR?ServiceKey="
-                    + "Nb%2BV2BcVQ%2BjSh4zZQkvreUtW0lbjoMq4kmUkR3Inc0OHZXmUTxvqXKaDhBoqvV0HGIx0%2BodRUS8K4Vyg7qiOwg%3D%3D"
+            URL url = new URL("http://openapi.airport.kr/openapi/service/StatusOfDepartures/getDeparturesCongestion?ServiceKey=" +
+                    "Nb%2BV2BcVQ%2BjSh4zZQkvreUtW0lbjoMq4kmUkR3Inc0OHZXmUTxvqXKaDhBoqvV0HGIx0%2BodRUS8K4Vyg7qiOwg%3D%3D"
             ); //검색 URL부분
 
             XmlPullParserFactory parserCreator = XmlPullParserFactory.newInstance(); // 위에서 생성된 URL을 통하여 서버에 요청하면 결과가 XML Resource로 전달됨
@@ -111,57 +119,24 @@ public class MainActivity extends Activity {
 
                 switch(parserEvent){
                     case XmlPullParser.START_TAG: //parser가 시작 태그를 만나면 실행
-                        if(parser.getName().equals("atime")){ //title 만나면 내용을 받을수 있게 하자
+                        if(parser.getName().equals("areadiv")){ //title 만나면 내용을 받을수 있게 하자
                             System.out.println("여기는 걸리나?");
-                            inAtime = true;
+                            inGateinfo1 = true;
                         }
-                        if(parser.getName().equals("t1sum1")){ //address 만나면 내용을 받을수 있게 하자
-                            inT1sum1 = true;
+                        if(parser.getName().equals("cgtdt")){ //address 만나면 내용을 받을수 있게 하자
+                            inCgtdt = true;
                         }
-                        if(parser.getName().equals("t1sum2")){ //mapx 만나면 내용을 받을수 있게 하자
-                            inT1sum2 = true;
+                        if(parser.getName().equals("cgthm")){ //address 만나면 내용을 받을수 있게 하자
+                            inCgthm = true;
                         }
-                        if(parser.getName().equals("t1sum3")){ //mapy 만나면 내용을 받을수 있게 하자
-                            inT1sum3 = true;
+                        if(parser.getName().equals("gateinfo2")){ //address 만나면 내용을 받을수 있게 하자
+                            inGateinfo2 = true;
                         }
-                        if(parser.getName().equals("t1sum4")){ //mapy 만나면 내용을 받을수 있게 하자
-                            inT1sum4 = true;
+                        if(parser.getName().equals("gateinfo3")){ //mapx 만나면 내용을 받을수 있게 하자
+                            inGateinfo3 = true;
                         }
-                        if(parser.getName().equals("t1sum5")){ //mapy 만나면 내용을 받을수 있게 하자
-                            inT1sum5 = true;
-                        }
-                        if(parser.getName().equals("t1sum6")){ //mapy 만나면 내용을 받을수 있게 하자
-                            inT1sum6 = true;
-                        }
-                        if(parser.getName().equals("t1sum7")){ //mapy 만나면 내용을 받을수 있게 하자
-                            inT1sum7 = true;
-                        }
-                        if(parser.getName().equals("t1sum8")){ //mapy 만나면 내용을 받을수 있게 하자
-                            inT1sum8 = true;
-                        }
-                        if(parser.getName().equals("t1sumset1")){ //mapy 만나면 내용을 받을수 있게 하자
-                            inT1sumset1 = true;
-                        }
-                        if(parser.getName().equals("t1sumset2")){ //mapy 만나면 내용을 받을수 있게 하자
-                            inT1sumset2 = true;
-                        }
-                        if(parser.getName().equals("t2sum1")){ //mapy 만나면 내용을 받을수 있게 하자
-                            inT2sum1 = true;
-                        }
-                        if(parser.getName().equals("t2sum2")){ //mapy 만나면 내용을 받을수 있게 하자
-                            inT2sum2 = true;
-                        }
-                        if(parser.getName().equals("t2sum3")){ //mapy 만나면 내용을 받을수 있게 하자
-                            inT2sum3 = true;
-                        }
-                        if(parser.getName().equals("t2sum4")){ //mapy 만나면 내용을 받을수 있게 하자
-                            inT2sum4 = true;
-                        }
-                        if(parser.getName().equals("t2sumset1")){ //mapy 만나면 내용을 받을수 있게 하자
-                            inT2sumset1 = true;
-                        }
-                        if(parser.getName().equals("t2sumset2")){ //mapy 만나면 내용을 받을수 있게 하자
-                            inT2sumset2 = true;
+                        if(parser.getName().equals("gateinfo4")){ //mapy 만나면 내용을 받을수 있게 하자
+                            inGateinfo4 = true;
                         }
                         if(parser.getName().equals("message")){ //message 태그를 만나면 에러 출력
                             status1.setText(status1.getText()+"에러");
@@ -170,92 +145,52 @@ public class MainActivity extends Activity {
                         break;
 
                     case XmlPullParser.TEXT://parser가 내용에 접근했을때
-
-                        if(inAtime){ //isTitle이 true일 때 태그의 내용을 저장.
-                            atime = parser.getText();
-                            inAtime = false;
+                        if(inAreadiv){ //isTitle이 true일 때 태그의 내용을 저장.
+                            areadiv = parser.getText();
+                            inAreadiv = false;
                         }
-                        if(inT1sum1){ //isAddress이 true일 때 태그의 내용을 저장.
-                            t1sum1 = parser.getText();
-                            inT1sum1 = false;
+                        if(inCgtdt){ //isTitle이 true일 때 태그의 내용을 저장.
+                            cgtdt = parser.getText();
+                            inCgtdt = false;
                         }
-                        if(inT1sum2){ //isMapx이 true일 때 태그의 내용을 저장.
-                            t1sum2 = parser.getText();
-                            inT1sum2 = false;
+                        if(inCgthm){ //isTitle이 true일 때 태그의 내용을 저장.
+                            cgthm = parser.getText();
+                            inCgthm = false;
                         }
-                        if(inT1sum3){ //isMapy이 true일 때 태그의 내용을 저장.
-                            t1sum3 = parser.getText();
-                            inT1sum3 = false;
+                        if(inGateinfo1){ //isTitle이 true일 때 태그의 내용을 저장.
+                            gateinfo1 = parser.getText();
+                            inGateinfo1 = false;
                         }
-                        if(inT1sum4){ //isMapy이 true일 때 태그의 내용을 저장.
-                            t1sum4 = parser.getText();
-                            inT1sum4 = false;
+                        if(inGateinfo2){ //isAddress이 true일 때 태그의 내용을 저장.
+                            gateinfo2 = parser.getText();
+                            inGateinfo2 = false;
                         }
-                        if(inT1sum5){ //isMapy이 true일 때 태그의 내용을 저장.
-                            t1sum5 = parser.getText();
-                            inT1sum5 = false;
+                        if(inGateinfo3){ //isMapx이 true일 때 태그의 내용을 저장.
+                            gateinfo3 = parser.getText();
+                            inGateinfo3 = false;
                         }
-                        if(inT1sum6){ //isMapy이 true일 때 태그의 내용을 저장.
-                            t1sum6 = parser.getText();
-                            inT1sum6 = false;
-                        }
-                        if(inT1sum7){ //isMapy이 true일 때 태그의 내용을 저장.
-                            t1sum7 = parser.getText();
-                            inT1sum7 = false;
-                        }
-                        if(inT1sum8){ //isMapy이 true일 때 태그의 내용을 저장.
-                            t1sum8 = parser.getText();
-                            inT1sum8 = false;
-                        }
-                        if(inT1sumset1){ //isMapy이 true일 때 태그의 내용을 저장.
-                            t1sumset1 = parser.getText();
-                            inT1sumset1 = false;
-                        }
-                        if(inT1sumset2){ //isMapy이 true일 때 태그의 내용을 저장.
-                            t1sumset2 = parser.getText();
-                            inT1sumset2 = false;
-                        }
-                        if(inT2sum1){ //isMapy이 true일 때 태그의 내용을 저장.
-                            t2sum1 = parser.getText();
-                            inT2sum1 = false;
-                        }
-                        if(inT2sum2){ //isMapy이 true일 때 태그의 내용을 저장.
-                            t2sum2 = parser.getText();
-                            inT2sum2 = false;
-                        }
-                        if(inT2sum3){ //isMapy이 true일 때 태그의 내용을 저장.
-                            t2sum3 = parser.getText();
-                            inT2sum3 = false;
-                        }
-                        if(inT2sum4){ //isMapy이 true일 때 태그의 내용을 저장.
-                            t2sum4 = parser.getText();
-                            inT2sum4 = false;
-                        }
-                        if(inT2sumset1){ //isMapy이 true일 때 태그의 내용을 저장.
-                            t2sumset1 = parser.getText();
-                            inT2sumset1 = false;
-                        }
-                        if(inT2sumset2){ //isMapy이 true일 때 태그의 내용을 저장.
-                            t2sumset2 = parser.getText();
-                            inT2sumset2 = false;
+                        if(inGateinfo4){ //isMapy이 true일 때 태그의 내용을 저장.
+                            gateinfo4 = parser.getText();
+                            inGateinfo4 = false;
                         }
                         break;
                     case XmlPullParser.END_TAG:
                         if(parser.getName().equals("item")){
-                            status1.setText(status1.getText()+" 시간대 : "+ atime + "\n T1입국장 동편(A,B) 현황: " + t1sum1 + "\n T1입국장 서편(E,F) 현황 : " + t1sum2
-                                    + "\n T1입국심사(C) 현황 : " + t1sum3 +  "\n T1입국심사(D) 현황 : " + t1sum4 + "\n T1출국장1,2 현황 : " + t1sum5
-                                    + "\n T1출국장3 현황 : " + t1sum6 + "\n T1출국장4 현황 : " + t1sum7 + "\n T1출국장5,6 현황 : " + t1sum8
-                                    + "\n T1입국장 합계 : " + t1sumset1 + "\n T1출국장 합계 : " + t1sumset2 + "\n T2입국장 1 현황 : " + t2sum1 + "\n T2입국장 2 현황 : " + t2sum2
-                                    + "\n T2출국당 1 현황 : " + t2sum3 + "\n T2 출국장 2 현황 : " + t2sum4 + "\n T2입국장 합계 : " +t2sumset1 + "\n T2출국장 합계 : " + t2sumset2 + "\n" );
-                            initem = false;
+
+                            status1.setText(status1.getText()+ gateinfo1 + "명");
+                            status2.setText(status2.getText()+gateinfo2 + "명");
+                            status3.setText(status3.getText()+gateinfo3 + "명");
+                            status4.setText(status4.getText()+gateinfo4 + "명");
+
                         }
                         break;
                 }
                 parserEvent = parser.next();
             }
-        } catch(Exception e){
+        } catch(Exception e) {
             status1.setText("에러가..났습니다...");
-        }*/
+
+        }
     }
 }
 
--- test/app/src/main/java/kr/ac/chosun/test/convenience.java
+++ test/app/src/main/java/kr/ac/chosun/test/convenience.java
@@ -21,5 +21,55 @@ public class convenience extends MainActivity {
                 startActivity(goeat); //액티비티 이동
             }
         });
+
+        ImageButton shopping = (ImageButton) findViewById(R.id.shopping_btn);
+        shopping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gotoshop = Uri.parse("https://www.airport.kr/ap/ko/shp/getDutyInfoMain.do");
+                Intent goshop = new Intent(Intent.ACTION_VIEW, gotoshop);
+                startActivity(goshop); //액티비티 이동
+            }
+        });
+
+        ImageButton bath = (ImageButton) findViewById(R.id.shopping_btn);
+        bath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gotobath = Uri.parse("https://www.airport.kr/ap/ko/svc/getFacilityMain.do");
+                Intent gobath = new Intent(Intent.ACTION_VIEW, gotobath);
+                startActivity(gobath); //액티비티 이동
+            }
+        });
+
+        ImageButton medical = (ImageButton) findViewById(R.id.medical_btn);
+        medical.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gotomedical = Uri.parse("https://www.airport.kr/ap/ko/svc/getFacilityMain.do");
+                Intent gomedical = new Intent(Intent.ACTION_VIEW, gotomedical);
+                startActivity(gomedical); //액티비티 이동
+            }
+        });
+
+        ImageButton telecom = (ImageButton) findViewById(R.id.telecom_btn);
+        telecom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gototelecom = Uri.parse("https://www.airport.kr/ap/ko/svc/getFacilityMain.do");
+                Intent gotelecom = new Intent(Intent.ACTION_VIEW, gototelecom);
+                startActivity(gotelecom); //액티비티 이동
+            }
+        });
+
+        ImageButton rest = (ImageButton) findViewById(R.id.rest_btn);
+        rest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gotorest = Uri.parse("https://www.airport.kr/ap/ko/svc/getFacilityMain.do");
+                Intent gorest = new Intent(Intent.ACTION_VIEW, gotorest);
+                startActivity(gorest); //액티비티 이동
+            }
+        });
     }
 }
