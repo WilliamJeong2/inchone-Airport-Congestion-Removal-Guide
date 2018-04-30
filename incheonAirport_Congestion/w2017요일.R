e6786706bf0eea6c6fdf045ce21547f4140aaf93
hth9021 <hth9021@gmail.com>
2018-04-30 17:39:23 +0900

    Rplot_Image_w2014_w2018

--- incheonAirport_Congestion/w2017요일.R
+++ incheonAirport_Congestion/w2017요일.R
@@ -1,39 +1,28 @@
-install.packages("readxl")
-install.packages("ggplot2")
+install.packages("readxl") # 엑셀 파일 읽을 수 있는 패키지 설치
+install.packages("ggplot2") # 파일 분석 하여 그래프로 출력하기 위한 패키지 설치
 
 library(readxl)
-w201701 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201701.xls")
-w201702 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201702.xls")
-w201702 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201702.xls")
-w201703 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201703.xls")
-w201704 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201704.xls")
-w201705 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201705.xls")
-w201706 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201706.xls")
-w201707 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201707.xls")
-w201708 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201708.xls")
-w201709 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201709.xls")
-w201710 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201710.xls")
-w201711 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201711.xls")
-w201712 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201712.xls")
+w201701 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201701.xls") #2017년 01월 요일 별 승객 인원 통계 엑셀 파일 가져오기
+w201702 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201702.xls") #2017년 02월 요일 별 승객 인원 통계 엑셀 파일 가져오기
+w201703 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201703.xls") #2017년 03월 요일 별 승객 인원 통계 엑셀 파일 가져오기
+w201704 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201704.xls") #2017년 04월 요일 별 승객 인원 통계 엑셀 파일 가져오기
+w201705 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201705.xls") #2017년 05월 요일 별 승객 인원 통계 엑셀 파일 가져오기
+w201706 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201706.xls") #2017년 06월 요일 별 승객 인원 통계 엑셀 파일 가져오기
+w201707 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201707.xls") #2017년 07월 요일 별 승객 인원 통계 엑셀 파일 가져오기
+w201708 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201708.xls") #2017년 08월 요일 별 승객 인원 통계 엑셀 파일 가져오기
+w201709 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201709.xls") #2017년 09월 요일 별 승객 인원 통계 엑셀 파일 가져오기
+w201710 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201710.xls") #2017년 10월 요일 별 승객 인원 통계 엑셀 파일 가져오기
+w201711 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201711.xls") #2017년 11월 요일 별 승객 인원 통계 엑셀 파일 가져오기
+w201712 <- read_excel("C:/Users/Han S.W/Desktop/airport/w201712.xls") #2017년 12월 요일 별 승객 인원 통계 엑셀 파일 가져오기
 
-w201701
-w201702
 
 library(ggplot2)
-w201701
-w201702
-w201703
-w201704
-w201705
-w201706
-w201707
-w201708
-w201709
-w201710
-w201711
-w201712
 
-ggplot(w201701, aes(x=요일, y=합계,group=1))+ggtitle("2017년 1월")+geom_line()+geom_line(aes(x=요일, y=출발), colour="red")+geom_line(aes(x=요일,y=도착),colour="blue")+geom_point()+geom_point(aes(x=요일,y=출발),colour="red")+geom_point(aes(x=요일,y=도착),colour="blue")
+ggplot(w201701, aes(x=요일, y=합계,group=1)) 
++ggtitle("2017년 1월")+geom_line()+geom_line(aes(x=요일, y=출발), colour="red")+geom_line(aes(x=요일,y=도착),colour="blue")
+
++geom_point()+geom_point(aes(x=요일,y=출발),colour="red")+geom_point(aes(x=요일,y=도착),colour="blue")
+
 ggplot(w201702, aes(x=요일, y=합계,group=1))+ggtitle("2017년 2월")+geom_line()+geom_line(aes(x=요일, y=출발), colour="red")+geom_line(aes(x=요일,y=도착),colour="blue")+geom_point()+geom_point(aes(x=요일,y=출발),colour="red")+geom_point(aes(x=요일,y=도착),colour="blue")
 ggplot(w201703, aes(x=요일, y=합계,group=1))+ggtitle("2017년 3월")+geom_line()+geom_line(aes(x=요일, y=출발), colour="red")+geom_line(aes(x=요일,y=도착),colour="blue")+geom_point()+geom_point(aes(x=요일,y=출발),colour="red")+geom_point(aes(x=요일,y=도착),colour="blue")
 ggplot(w201704, aes(x=요일, y=합계,group=1))+ggtitle("2017년 4월")+geom_line()+geom_line(aes(x=요일, y=출발), colour="red")+geom_line(aes(x=요일,y=도착),colour="blue")+geom_point()+geom_point(aes(x=요일,y=출발),colour="red")+geom_point(aes(x=요일,y=도착),colour="blue")
