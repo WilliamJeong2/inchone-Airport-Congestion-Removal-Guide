cad77d7ce854bc63cb32c64038570b362fc23c14
WilliamJeong2 <wjd5480@gmail.com>
2018-05-01 11:01:09 +0900

    요일별 혼잡도 구현 ver 1.0

--- test/app/src/main/java/kr/ac/chosun/test/congestion.java
+++ test/app/src/main/java/kr/ac/chosun/test/congestion.java
@@ -1,11 +1,49 @@
 package kr.ac.chosun.test;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 
 public class congestion extends MainActivity {
+
+    private ImageView image;
+    private String[] states;
+    private Spinner spinner;
+    private TypedArray imgs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.congestion); //해당 아이디에 자신이 만든 레이아웃의 이름을 쓴다
+
+        states = getResources().getStringArray(R.array.week_list);
+        imgs = getResources().obtainTypedArray(R.array.week_flag_list);
+
+        image = (ImageView) findViewById(R.id.imageview1);
+        spinner = (Spinner) findViewById(R.id.spinner1);
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, states);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                image.setImageResource(imgs.getResourceId(spinner.getSelectedItemPosition(),-1));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        };
     }
-}
