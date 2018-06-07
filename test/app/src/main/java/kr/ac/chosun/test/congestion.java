994a70a2b862b816d88e95d86d21bf2e0a2959ff
WilliamJeong2 <wjd5480@gmail.com>
2018-06-07 19:50:02 +0900

    코드 재배

--- test/app/src/main/java/kr/ac/chosun/test/congestion.java
+++ test/app/src/main/java/kr/ac/chosun/test/congestion.java
@@ -1,7 +1,5 @@
 package kr.ac.chosun.test;
 
-import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,24 +10,7 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.github.mikephil.charting.animation.Easing;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import uk.co.senab.photoview.PhotoViewAttacher;
-
-
-
 
 public class congestion extends AppCompatActivity {
 
@@ -38,70 +19,10 @@ public class congestion extends AppCompatActivity {
     String choice_month = ""; //선택된 월이 들어갈 string 변수 선언
     PhotoViewAttacher mAttacher;
 
-    private LineChart lineChart;
-
-
     @Override
     protected void onCreate(Bundle savedInstaceState) {
         super.onCreate(savedInstaceState);
         setContentView(R.layout.congestion);
-
-        lineChart = (LineChart)findViewById(R.id.chart);
-
-        List<Entry> entries = new ArrayList<>();
-        entries.add(new Entry(1, 3108702));
-        entries.add(new Entry(2, 2947987));
-        entries.add(new Entry(3, 2576238));
-        entries.add(new Entry(4, 2699330));
-        entries.add(new Entry(5, 2580591));
-        entries.add(new Entry(6, 2768551));
-        entries.add(new Entry(7, 3084710));
-        entries.add(new Entry(8, 3180091));
-        entries.add(new Entry(9, 2903792));
-        entries.add(new Entry(10, 2750012));
-        entries.add(new Entry(11, 2912331));
-        entries.add(new Entry(12, 3038459));
-
-
-        LineDataSet lineDataSet = new LineDataSet(entries, "2018년 월별 이용객 수");
-        lineDataSet.setLineWidth(2);
-        lineDataSet.setCircleRadius(6);
-        lineDataSet.setCircleColor(Color.parseColor("#FFA1B4DC"));
-        lineDataSet.setCircleColorHole(Color.BLUE);
-        lineDataSet.setColor(Color.parseColor("#FFA1B4DC"));
-        lineDataSet.setDrawCircleHole(true);
-        lineDataSet.setDrawCircles(true);
-        lineDataSet.setDrawHorizontalHighlightIndicator(false);
-        lineDataSet.setDrawHighlightIndicators(false);
-        lineDataSet.setDrawValues(false);
-
-        LineData lineData = new LineData(lineDataSet);
-        lineChart.setData(lineData);
-
-        XAxis xAxis = lineChart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setTextColor(Color.BLACK);
-        xAxis.enableGridDashedLine(12, 36, 0);
-
-        YAxis yLAxis = lineChart.getAxisLeft();
-        yLAxis.setTextColor(Color.BLACK);
-
-        YAxis yRAxis = lineChart.getAxisRight();
-        yRAxis.setDrawLabels(false);
-        yRAxis.setDrawAxisLine(false);
-        yRAxis.setDrawGridLines(false);
-
-        Description description = new Description();
-        description.setText("");
-
-        lineChart.setDoubleTapToZoomEnabled(false);
-        lineChart.setDrawGridBackground(false);
-        lineChart.setDescription(description);
-        lineChart.animateY(2000, Easing.EasingOption.EaseInCubic);
-        lineChart.invalidate();
-
-
-
 
         final Spinner spin1 = (Spinner)findViewById(R.id.spinner1);
         final Spinner spin2 = (Spinner)findViewById(R.id.spinner2);
--- test/app/src/main/java/kr/ac/chosun/test/delay.java
+++ test/app/src/main/java/kr/ac/chosun/test/delay.java
@@ -1,11 +1,81 @@
 package kr.ac.chosun.test;
 
 import android.os.Bundle;
+import android.graphics.Color;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class delay extends MainActivity {
+
+    private LineChart lineChart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.delay); //해당 아이디에 자신이 만든 레이아웃의 이름을 쓴다
+
+        lineChart = (LineChart)findViewById(R.id.chart);
+
+        List<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(1, 3108702));
+        entries.add(new Entry(2, 2947987));
+        entries.add(new Entry(3, 2576238));
+        entries.add(new Entry(4, 2699330));
+        entries.add(new Entry(5, 2580591));
+        entries.add(new Entry(6, 2768551));
+        entries.add(new Entry(7, 3084710));
+        entries.add(new Entry(8, 3180091));
+        entries.add(new Entry(9, 2903792));
+        entries.add(new Entry(10, 2750012));
+        entries.add(new Entry(11, 2912331));
+        entries.add(new Entry(12, 3038459));
+
+
+        LineDataSet lineDataSet = new LineDataSet(entries, "2018년 월별 이용객 수");
+        lineDataSet.setLineWidth(2);
+        lineDataSet.setCircleRadius(6);
+        lineDataSet.setCircleColor(Color.parseColor("#FFA1B4DC"));
+        lineDataSet.setCircleColorHole(Color.BLUE);
+        lineDataSet.setColor(Color.parseColor("#FFA1B4DC"));
+        lineDataSet.setDrawCircleHole(true);
+        lineDataSet.setDrawCircles(true);
+        lineDataSet.setDrawHorizontalHighlightIndicator(false);
+        lineDataSet.setDrawHighlightIndicators(false);
+        lineDataSet.setDrawValues(false);
+
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.enableGridDashedLine(12, 36, 0);
+
+        YAxis yLAxis = lineChart.getAxisLeft();
+        yLAxis.setTextColor(Color.BLACK);
+
+        YAxis yRAxis = lineChart.getAxisRight();
+        yRAxis.setDrawLabels(false);
+        yRAxis.setDrawAxisLine(false);
+        yRAxis.setDrawGridLines(false);
+
+        Description description = new Description();
+        description.setText("");
+
+        lineChart.setDoubleTapToZoomEnabled(false);
+        lineChart.setDrawGridBackground(false);
+        lineChart.setDescription(description);
+        lineChart.animateY(0, Easing.EasingOption.EaseInCubic);
+        lineChart.invalidate();
+
     }
 }
